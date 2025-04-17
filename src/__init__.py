8e8a99efa72390c0ffa8630e10213342c9ab41e6
marceloriv <ma.riveracontreras@duocuc.com>
2025-04-17 17:40:52 -0400

    Modificar la función suma para aceptar parámetros y agregar su uso en main.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
-def suma():
-    return 1 + 1
+def suma(n1, n2):
+    return n1 + n2
 
 
 def resta():
---
+++ src/main.py
@@ -0,0 +1,6 @@
+from __init__ import suma
+
+n1 = int(input("Ingrese el primer numero: "))
+n2 = int(input("Ingrese el segundo numero: "))
+
+print("Suma: ", suma(n1, n2))
