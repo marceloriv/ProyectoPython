e10d09a5f2eac71f80aef01cf3bb56b5a93f4ce9
Sebastian N. <marcelonovoanav@gmail.com>
2025-04-17 17:24:02 -0400

    estoy dentro

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
 def suma():
     return 1 + 1
-def potencia(n1, n2):
-    return n1 ** n2
+
+def resta():
+    return 1- 1
+
+def multiplicacion():
+    return 2 * 2
+
+def division():
+    return 2/2
