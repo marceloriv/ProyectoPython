c60e1596685fe406c9da9d31512d9c0465ba32ee
marceloriv <ma.riveracontreras@duocuc.com>
2025-04-17 17:28:41 -0400

    Agregar funciones matemáticas básicas en __init__.py y eliminar duplicados en main.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,14 @@
-# This file is intentionally left blank.
-
 def suma():
-    return
+    return 1 + 1
+
+
+def resta():
+    return 1 - 1
+
+
+def multiplicacion():
+    return 2 * 2
+
+
+def division():
+    return 2 / 2
